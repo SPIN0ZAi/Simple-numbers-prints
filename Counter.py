ae982ce608df5a2b7a2b6f36f01de8017a818435
SPIN0ZAi <109903730+SPIN0ZAi@users.noreply.github.com>
2025-04-10 20:48:36 +0200

    Initial commit

---
+++ Counter.py
@@ -0,0 +1,16 @@
+number = 1
+
+for i in range(4):
+     for j in range (3 - i):
+      print(" ",end="")
+
+     for k in range(i + 1):
+       print(number, end=" ")
+       number += 1
+
+     print()
+
+
+
+
+
